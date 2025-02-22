fd667e7c7b9b914dbcf180ae5931f0e191140e51
Luffy <aladdinn360@gmail.com>
2025-02-22 05:03:56 +0100

    test commit from VS2022

--- Main.cpp
+++ Main.cpp
@@ -2,13 +2,6 @@
 
 #include "Core/Application.h"
 
-#include "imgui.h"
-#include "imgui_impl_win32.h"
-#include "imgui_impl_dx12.h"
-#include <d3d12.h>
-#include <dxgi1_4.h>
-#include <tchar.h>
-
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
