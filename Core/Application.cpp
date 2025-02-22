0a010549e16e7a56cfa46e2335d77b7a705a85c6
Luffy <aladdinn360@gmail.com>
2025-02-22 19:55:10 +0100

    quickly initilize DX12 (only clear buffer)

--- Core/Application.cpp
+++ Core/Application.cpp
@@ -1,15 +1,154 @@
 #include "stdafx.h"
 #include "Application.h"
 
+#include <iostream>
+
 namespace Core
 {
   Application::Application(UINT width, UINT height, std::wstring name)
     : DirectXApplication(width, height, name)
+    , m_rtvDescriptorSize(0)
+    , m_frameIndex(0)
   {
   }
 
   void Application::OnInit()
   {
+    // Load pipeline
+
+    unsigned int dxgiFactoryFlags = 0;
+
+#if defined(_DEBUG)
+    // Enable the debug layer (requires the Graphics Tools "optional feature").
+    // NOTE: Enabling the debug layer after device creation will invalidate the active device.
+    {
+      ComPtr<ID3D12Debug> debugController;
+      if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+      {
+        debugController->EnableDebugLayer();
+
+        // Enable additional debug layers.
+        dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+      }
+    }
+#endif
+
+    // Create Factory
+    ComPtr<IDXGIFactory4> factory;
+    ThrowIfFailed(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory)));
+
+    // Create device
+    {
+      ComPtr<IDXGIAdapter> warpAdapter;
+      ComPtr<IDXGIAdapter1> hardwareAdapter;
+
+      if (m_useWarpDevice)
+        ThrowIfFailed(factory->EnumWarpAdapter(IID_PPV_ARGS(&warpAdapter)));
+      else
+        GetHardwareAdapter(factory.Get(), &hardwareAdapter);
+
+      ThrowIfFailed(D3D12CreateDevice(
+        m_useWarpDevice ? warpAdapter.Get() : hardwareAdapter.Get(),
+        D3D_FEATURE_LEVEL_11_0,
+        IID_PPV_ARGS(&m_device)
+      ));
+    }
+
+    // Describe and create the command queue.
+    {
+      D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+      queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+      queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+      ThrowIfFailed(m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue)));
+    }
+    
+    // Describe and create the swap chain.
+    {
+      DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+      swapChainDesc.BufferCount = FrameCount;
+      swapChainDesc.Width = m_width;
+      swapChainDesc.Height = m_height;
+      swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+      swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+      swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+      swapChainDesc.SampleDesc.Count = 1;
+
+      ComPtr<IDXGISwapChain1> swapChain;
+      ThrowIfFailed(factory->CreateSwapChainForHwnd(
+        m_commandQueue.Get(),        // Swap chain needs the queue so that it can force a flush on it.
+        WindowsApplication::GetHwnd(),
+        &swapChainDesc,
+        nullptr,
+        nullptr,
+        &swapChain
+      ));
+
+      ThrowIfFailed(swapChain.As(&m_swapChain));
+      m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+    }
+
+    // full screen transitions not supported.
+    ThrowIfFailed(factory->MakeWindowAssociation(WindowsApplication::GetHwnd(), DXGI_MWA_NO_ALT_ENTER));
+
+    // Create descriptor heaps.
+    {
+      // Describe and create a render target view (RTV) descriptor heap.
+      D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+      rtvHeapDesc.NumDescriptors = FrameCount;
+      rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+      rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+      ThrowIfFailed(m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap)));
+      m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    }
+
+    // Create frame resources.
+    {
+      CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+
+      // Create a RTV for each frame.
+      for (UINT n = 0; n < FrameCount; n++)
+      {
+        ThrowIfFailed(m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n])));
+        m_device->CreateRenderTargetView(m_renderTargets[n].Get(), nullptr, rtvHandle);
+        rtvHandle.Offset(1, m_rtvDescriptorSize);
+        ThrowIfFailed(m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocators[n])));
+      }
+    }
+
+    // Create the command list.
+    for (unsigned n = 0; n < FrameCount; ++n)
+      ThrowIfFailed(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocators[n].Get(), nullptr, IID_PPV_ARGS(&m_commandList)));
+
+    // Close the command list and execute it to begin the initial GPU setup.
+    ThrowIfFailed(m_commandList->Close());
+    ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
+    m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+
+    // Create synchronization objects.
+    {
+      ThrowIfFailed(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)));
+      m_fenceValues[m_frameIndex]++;
+
+      // Create an event handle to use for frame synchronization.
+      m_fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+      if (m_fenceEvent == nullptr)
+      {
+        ThrowIfFailed(HRESULT_FROM_WIN32(GetLastError()));
+      }
+
+      // Wait for the command list to execute; we are reusing the same command 
+      // list in our main loop but for now, we just want to wait for setup to 
+      // complete before continuing.
+      // Schedule a Signal command in the queue.
+      ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), m_fenceValues[m_frameIndex]));
+
+      // Wait until the fence has been processed.
+      ThrowIfFailed(m_fence->SetEventOnCompletion(m_fenceValues[m_frameIndex], m_fenceEvent));
+      WaitForSingleObjectEx(m_fenceEvent, INFINITE, FALSE);
+
+      // Increment the fence value for the current frame.
+      m_fenceValues[m_frameIndex]++;
+    }
   }
 
   void Application::OnUpdate()
@@ -18,9 +157,76 @@ namespace Core
 
   void Application::OnRender()
   {
+    // Populate Command list
+    // Command list allocators can only be reset when the associated 
+    // command lists have finished execution on the GPU; apps should use 
+    // fences to determine GPU execution progress.
+    ThrowIfFailed(m_commandAllocators[m_frameIndex]->Reset());
+
+    // However, when ExecuteCommandList() is called on a particular command 
+    // list, that command list can then be reset at any time and must be before 
+    // re-recording.
+    ThrowIfFailed(m_commandList->Reset(m_commandAllocators[m_frameIndex].Get(), nullptr));
+
+    auto barrier1 = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+    // Indicate that the back buffer will be used as a render target.
+    m_commandList->ResourceBarrier(1, &barrier1);
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
+    m_commandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
+
+    // Record commands.
+    const float clearColor[] = { 0.25f, 0.25f, 0.45f, 1.0f };
+    m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+
+    auto barrier2 = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+    // Indicate that the back buffer will now be used to present.
+    m_commandList->ResourceBarrier(1, &barrier2);
+
+    ThrowIfFailed(m_commandList->Close());
+
+
+    ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
+    m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+
+    // Present the frame.
+    ThrowIfFailed(m_swapChain->Present(1, 0));
+
+    // MOVE TO NEXT FRAME
+    // Signal and increment the fence value.
+    const UINT64 currentFenceValue = m_fenceValues[m_frameIndex];
+    ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), currentFenceValue));
+
+    m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+
+    // If the next frame is not ready to be rendered yet, wait until
+    if (m_fence->GetCompletedValue() < m_fenceValues[m_frameIndex])
+    {
+      ThrowIfFailed(m_fence->SetEventOnCompletion(m_fenceValues[m_frameIndex], m_fenceEvent));
+      WaitForSingleObjectEx(m_fenceEvent, INFINITE, false);
+    }
+
+    m_fenceValues[m_frameIndex] = currentFenceValue + 1;
   }
 
   void Application::OnDestroy()
   {
+    // Ensure that the GPU is no longer referencing resources that are about to be
+    // cleaned up by the destructor.
+
+    // Wait for the command list to execute; we are reusing the same command 
+    // list in our main loop but for now, we just want to wait for setup to 
+    // complete before continuing.
+    // Schedule a Signal command in the queue.
+    ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), m_fenceValues[m_frameIndex]));
+
+    // Wait until the fence has been processed.
+    ThrowIfFailed(m_fence->SetEventOnCompletion(m_fenceValues[m_frameIndex], m_fenceEvent));
+    WaitForSingleObjectEx(m_fenceEvent, INFINITE, FALSE);
+
+    // Increment the fence value for the current frame.
+    m_fenceValues[m_frameIndex]++;
+
+    CloseHandle(m_fenceEvent);
   }
 }
--- Core/Application.h
+++ Core/Application.h
@@ -16,6 +16,29 @@ namespace Core
     virtual void OnUpdate() override;
     virtual void OnRender() override;
     virtual void OnDestroy() override;
+
+  private:
+    static const uint32_t FrameCount = 2;
+
+    // Pipeline objects
+    ComPtr<ID3D12Device> m_device;
+    ComPtr<ID3D12CommandQueue> m_commandQueue;
+    ComPtr<IDXGISwapChain3> m_swapChain;
+    // render target heap
+    ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+    unsigned int m_rtvDescriptorSize;
+    ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
+    ComPtr<ID3D12CommandAllocator> m_commandAllocators[FrameCount];
+    ComPtr<ID3D12GraphicsCommandList> m_commandList;
+    ComPtr<ID3D12RootSignature> m_rootSignature;
+    ComPtr<ID3D12PipelineState> m_pipelineState;
+
+    // Synchronization objects.
+    uint32_t m_frameIndex;
+    HANDLE m_fenceEvent;
+    ComPtr<ID3D12Fence> m_fence;
+    uint64_t m_fenceValues[FrameCount];
+
   };
 }
 
