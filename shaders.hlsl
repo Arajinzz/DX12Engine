81e2e784d6cbd4b96e5374849bf0fa5e0299568a
Luffy <aladdinn360@gmail.com>
2025-03-22 00:26:06 +0100

    simple diffuse lighting

--- shaders.hlsl
+++ shaders.hlsl
@@ -24,6 +24,11 @@ struct PSInput
     float2 uv : TEXCOORD;
 };
 
+float remap(float value, float inMin, float inMax, float outMin, float outMax)
+{
+    return outMin + (value - inMin) * (outMax - outMin) / (inMax - inMin);
+}
+
 PSInput VSMain(float4 position : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
 {
     PSInput result;
@@ -34,7 +39,7 @@ PSInput VSMain(float4 position : POSITION, float3 normal : NORMAL, float2 uv : T
     float4 clipPos = mul(viewPos, s_projection);
     
     result.position = clipPos;
-    result.normal = normal;
+    result.normal = mul(float4(normal, 0.0), m_model).xyz;
     result.uv = uv;
 
     return result;
@@ -42,5 +47,25 @@ PSInput VSMain(float4 position : POSITION, float3 normal : NORMAL, float2 uv : T
 
 float4 PSMain(PSInput input) : SV_TARGET
 {
-    return g_texture.Sample(g_sampler, input.uv) * float4(input.normal, 1.0);
+    float3 baseColor = float3(0.5f, 0.5f, 0.5f);
+    float3 lighting = float3(0.0f, 0.0f, 0.0f);
+    float3 normal = normalize(input.normal);
+    float3 ambient = float3(0.5f, 0.5f, 0.5f);
+    float3 skyColor = float3(0.0f, 0.3f, 0.6f);
+    float3 groundColor = float3(0.6f, 0.3f, 0.1f);
+    
+    float hemiMix = remap(normal.y, -1.0, 1.0, 0.0, 1.0f);
+    float3 hemi = lerp(groundColor, skyColor, hemiMix);
+    
+    // diffuse
+    float3 lightDir = normalize(float3(1.0f, 1.0f, 1.0f));
+    float3 lightColor = float3(1.0f, 1.0f, 0.9f);
+    float diffuse = max(0.0f, dot(lightDir, normal)) * lightColor;
+    
+    lighting = ambient * 0.5 + hemi * 0.5 + diffuse * 0.5;
+    
+    // gamma corrected
+    float3 color = pow(baseColor * lighting, float3(1.0f / 2.2f, 1.0f / 2.2f, 1.0f / 2.2f));
+    
+    return float4(color, 1.0);
 }
