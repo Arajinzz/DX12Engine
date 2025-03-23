6ac161701d42b5e2624eafd2942d261f91940c70
Luffy <aladdinn360@gmail.com>
2025-03-23 23:48:10 +0100

    add mips generator class

---
+++ Core/DX12MipsGenerator.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "DX12MipsGenerator.h"
+
+
+namespace Core
+{
+  DX12MipsGenerator::DX12MipsGenerator()
+  {
+  }
+
+  DX12MipsGenerator::~DX12MipsGenerator()
+  {
+  }
+}
