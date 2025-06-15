ee9e1a4365cd1f4290530a9c4260190575c50305
Arajinzz <69687886+Arajinzz@users.noreply.github.com>
2025-06-15 20:12:44 +0200

    refactoring: introduce DX12Interface singleton

---
+++ DX12Interface.h
@@ -0,0 +1,50 @@
+#pragma once
+
+using namespace DirectX;
+using Microsoft::WRL::ComPtr;
+
+namespace Core
+{
+  // This will provide functions to manage DX12,
+// like creating buffers, heaps, root signatures ...etc.
+// It will be a singleton.
+  class DX12Interface
+  {
+  public:
+    static DX12Interface& Get()
+    {
+      static DX12Interface instance;
+      return instance;
+    }
+
+    inline ID3D12Device* GetDevice()
+    {
+      return m_device.Get();
+    }
+
+    inline IDXGIFactory4* GetFactory()
+    {
+      return m_factory.Get();
+    }
+
+    ~DX12Interface();
+
+  protected:
+    void Initialize();
+
+  private:
+    void GetHardwareAdapter(
+      _In_ IDXGIFactory1* pFactory,
+      _Outptr_result_maybenull_ IDXGIAdapter1** ppAdapter,
+      bool requestHighPerformanceAdapter = false);
+
+  private:
+    ComPtr<ID3D12Device> m_device;
+    ComPtr<IDXGIFactory4> m_factory;
+
+  private:
+    DX12Interface();
+    DX12Interface(const DX12Interface&) = delete;
+    DX12Interface& operator=(const DX12Interface&) = delete;
+  };
+}
