868a2aaf12e718e14f571e2cbc3f31123e5af2ba
Arajinzz <69687886+Arajinzz@users.noreply.github.com>
2025-07-05 00:51:54 +0200

    added tinyobjloader

--- Core/Application.cpp
+++ Core/Application.cpp
@@ -12,6 +12,11 @@
 #include <json.hpp>
 using json = nlohmann::json;
 
+#define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
+// Optional. define TINYOBJLOADER_USE_MAPBOX_EARCUT gives robust triangulation. Requires C++11
+//#define TINYOBJLOADER_USE_MAPBOX_EARCUT
+#include "tiny_obj_loader.h"
+
 // TODO: better ERROR handling
 // for example if a texture is not available like skybox
 // Program exits silently without any error
